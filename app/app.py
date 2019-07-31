92894c7f29e29d496e5057aed0ffeecc2126d6db
brap <doctor.fun107@gmail.com>
2019-07-31 21:49:35 +0100

    moved some json around, added latest merged mpdata

--- app/app.py
+++ app/app.py
@@ -500,7 +500,8 @@ def main():
 
 @app.route('/test')
 def test():
-    return getMPColour(120236641)
+    updateDB()
+    return print('database updated')
 
 if __name__ == "__main__":
     app.run(debug=True)
